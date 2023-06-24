cfa4e366e6b7c93a087d2bd7385fb9d1360903d2
isnancahyadi <isnan.arifc@gmail.com>
2023-06-24 14:55:17 +0700

    [ADD] integrate register to API, [FIX] UI register

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,8 +1,79 @@
 import Head from "next/head";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { useRouter } from "next/router";
 
 const Register = () => {
+  const router = useRouter();
+
+  const [fullname, setFullname] = useState("");
+  const [jobTitle, setJobTitle] = useState("");
+  const [company, setCompany] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNum, setPhoneNum] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPass, setConfirmPass] = useState("");
+
+  const handleRegister = async (event) => {
+    event.preventDefault();
+
+    if (confirmPass === password) {
+      const payload = {
+        email,
+        password: confirmPass,
+        fullname,
+        company,
+        job_title: jobTitle,
+        phone: phoneNum,
+      };
+
+      Swal.fire({
+        title: "Harap tunggu...",
+        allowOutsideClick: false,
+        didOpen: () => Swal.showLoading(),
+      });
+
+      await axios
+        .post(process.env.NEXT_PUBLIC_REGISTER, payload)
+        .then(() => {
+          Swal.fire({
+            title: "Register Berhasil",
+            text: "Register berhasil. Harap login untuk menggunakan aplikasi.",
+            icon: "success",
+          }).then(() => {
+            router.replace("/login");
+          });
+        })
+        .catch(({ response }) => {
+          const getRes = Object.keys(response?.data?.messages);
+
+          let msgProperty = [];
+
+          getRes.map((item, key) => {
+            const {
+              [item]: { message },
+            } = response?.data?.messages;
+
+            msgProperty[key] = message;
+          });
+
+          Swal.fire({
+            title: "Register Gagal",
+            text: msgProperty.toString().split(".,").join(", "),
+            icon: "error",
+          });
+        });
+    } else {
+      Swal.fire({
+        title: "Register Gagal",
+        text: "Password tidak sama.",
+        icon: "error",
+      });
+    }
+  };
+
   return (
     <>
       <Head>
@@ -45,19 +116,40 @@ const Register = () => {
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
                   euismod ipsum et dui rhoncus auctor.
                 </p>
-                <form
-                  onSubmit={(event) => {
-                    event.preventDefault();
-                  }}
-                >
+                <form onSubmit={handleRegister}>
                   <div className="mb-3">
                     <label className="form-label">Nama</label>
                     <input
                       type="text"
                       className="input-register form-control"
                       placeholder="Masukkan nama lengkap"
-                      // onChange={(e) => setUsername(e.target.value)}
+                      onChange={(e) => setFullname(e.target.value)}
+                      required
                     />
+                  </div>
+                  <div className="mb-3">
+                    <div className="row">
+                      <div className="col-md-6">
+                        <label className="form-label">Pekerjaan</label>
+                        <input
+                          type="text"
+                          className="input-register form-control"
+                          placeholder="Masukkan pekerjaan saat ini"
+                          onChange={(e) => setJobTitle(e.target.value)}
+                          required
+                        />
+                      </div>
+                      <div className="col-md-6">
+                        <label className="form-label">Perusahaan</label>
+                        <input
+                          type="text"
+                          className="input-register form-control"
+                          placeholder="Masukkan perusahaan saat ini"
+                          onChange={(e) => setCompany(e.target.value)}
+                          required
+                        />
+                      </div>
+                    </div>
                   </div>
                   <div className="mb-3">
                     <label className="form-label">Email</label>
@@ -65,7 +157,8 @@ const Register = () => {
                       type="text"
                       className="input-register form-control"
                       placeholder="Masukkan alamat email"
-                      // onChange={(e) => setUsername(e.target.value)}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
                     />
                   </div>
                   <div className="mb-3">
@@ -74,33 +167,40 @@ const Register = () => {
                       type="text"
                       className="input-register form-control"
                       placeholder="Masukkan no. handphone"
-                      // onChange={(e) => setUsername(e.target.value)}
+                      onChange={(e) => setPhoneNum(e.target.value)}
+                      required
                     />
                   </div>
                   <div className="mb-3">
-                    <label className="form-label">Kata Sandi</label>
-                    <input
-                      type="password"
-                      className="input-register form-control"
-                      placeholder="Masukkan kata sandi"
-                      // onChange={(e) => setPassword(e.target.value)}
-                    />
-                  </div>
-                  <div className="mb-3">
-                    <label className="form-label">Konfirmasi Kata Sandi</label>
-                    <input
-                      type="password"
-                      className="input-register form-control"
-                      placeholder="Masukkan konfirmasi kata sandi"
-                      // onChange={(e) => setUsername(e.target.value)}
-                    />
+                    <div className="row">
+                      <div className="col-md-6">
+                        <label className="form-label">Kata Sandi</label>
+                        <input
+                          type="password"
+                          className="input-register form-control"
+                          placeholder="Masukkan kata sandi"
+                          onChange={(e) => setPassword(e.target.value)}
+                          required
+                        />
+                      </div>
+                      <div className="col-md-6">
+                        <label className="form-label">
+                          Konfirmasi Kata Sandi
+                        </label>
+                        <input
+                          type="password"
+                          className="input-register form-control"
+                          placeholder="Masukkan konfirmasi kata sandi"
+                          onChange={(e) => setConfirmPass(e.target.value)}
+                        />
+                      </div>
+                    </div>
                   </div>
                   <div className="d-grid">
                     <button
                       id="btn-register"
                       type="submit"
                       className="btn btn-tertiary mt-4 fw-semibold"
-                      // onClick={handleLogin}
                     >
                       Daftar
                     </button>
